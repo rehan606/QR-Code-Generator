a492362653cea3d21e3eadf8f7b1c365773a90cf
Rehan <rehanlemu@gmail.com>
2025-11-19 23:28:31 +0600

    Complete GEnerate QR code

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const handleGenerateQrCode = () => {
     const url=document.getElementById('text-input').value.trim();
     if(!url) {
         return alert("Please enter a valid text or URL");
-        
+    }
+    document.getElementById('text-input').value="";
+
+    try {
+        new QRCode(document.getElementById("qr-code"), {
+            text: url,
+            width: 200,
+            height: 200,
+            colorDark : "#000000ff",
+            colorLight : "#ffffff",
+            correctLevel : QRCode.CorrectLevel.H
+            
+        }); 
+    } 
+    catch (error) {
+        console.error("Error generating QR code:", error);
+        alert("Failed to generate QR code. Please try again.");
     }
 }
