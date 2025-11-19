047787f52e37631e24bb0911a6823ea12ee4455e
Rehan <rehanlemu@gmail.com>
2025-11-19 23:02:52 +0600

    create onCLick function

---
+++ app.js
@@ -0,0 +1,7 @@
+const handleGenerateQrCode = () => {
+    const url=document.getElementById('text-input').value.trim();
+    if(!url) {
+        return alert("Please enter a valid text or URL");
+        
+    }
+}
