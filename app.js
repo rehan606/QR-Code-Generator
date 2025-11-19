061a28dde1bd73976474cca85925621331e04224
Rehan <rehanlemu@gmail.com>
2025-11-20 02:33:19 +0600

    added QR code download function

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const handleGenerateQrCode = () => {
             correctLevel : QRCode.CorrectLevel.H
             
         }); 
-        
+
         alert("Successfully generated QR code!");
         document.getElementById('text-input').value="";
 
@@ -28,4 +28,34 @@ const handleGenerateQrCode = () => {
         console.error("Error generating QR code:", error);
         alert("Failed to generate QR code. Please try again.");
     }
-}
+};
+
+
+
+let qrImageData = "";
+
+    function generateQR() {
+      const text = document.getElementById("text-input").value;
+      if (!text) {
+        alert("Please enter some text!");
+        return;
+      }
+
+      QRCode.toDataURL(text, { width: 300 }, function (err, url) {
+        if (err) console.error(err);
+
+        qrImageData = url; // save for download
+
+        document.getElementById("qr-code").innerHTML =
+          `<img src="${url}" width="250">`;
+
+        document.getElementById("downloadBtn").style.display = "block";
+      });
+    }
+
+    function downloadQR() {
+      const link = document.createElement("a");
+      link.href = qrImageData;
+      link.download = "qr-code.png";
+      link.click();
+    }
