6b96bbb264d7f57875c0eee5d14330d7b5dc25d0
Rehan <rehanlemu@gmail.com>
2025-11-20 02:02:41 +0600

    successfully update UI

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ const handleGenerateQrCode = () => {
     if(!url) {
         return alert("Please enter a valid text or URL");
     }
-    document.getElementById('text-input').value="";
+
+    // Clear old QR
+    document.getElementById("qr-code").innerHTML = "";
+
+    
 
     try {
         new QRCode(document.getElementById("qr-code"), {
             text: url,
-            width: 200,
-            height: 200,
+            width: 130,
+            height: 130,
             colorDark : "#000000ff",
             colorLight : "#ffffff",
             correctLevel : QRCode.CorrectLevel.H
             
         }); 
+        
+        alert("Successfully generated QR code!");
+        document.getElementById('text-input').value="";
+
     } 
     catch (error) {
         console.error("Error generating QR code:", error);
